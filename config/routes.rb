e4e24e19e4cba9f65939507613df9085a93fbda2
Bry Cox <carl.cc@gmail.com>
2019-08-22 17:28:56 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   post '/login' => 'sessions#create'
+  get '/welcome' => 'welcome#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
